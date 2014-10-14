d3b983f53e667bfb7d947c8d6603475572a2d0d1
Simply007 <430596@mail.muni.cz>
2014-10-14 20:48:04 +0200

    Sample Signal R commit

---
+++ SampleSignalR/Hubs/TestHub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace MVC5SignalRChat.Hubs
+{
+    public class TestHub : Hub
+    {
+
+        private static Dictionary<string, Coordinates> Storage = new Dictionary<string, Coordinates>();
+
+        public void Send(string id, double latitude, double longtitude)
+        {
+            var generator = new Random();
+            Storage[id] = new Coordinates { Latitude = latitude + generator.Next(1, 10), Longtitude = longtitude + generator.Next(1, 10) };
+            Clients.All.addNewMessageToPage(JsonConvert.SerializeObject(Storage, Formatting.Indented));
+        }
+
+        private class Coordinates
+        {
+            public double Latitude { get; set; }
+            public double Longtitude { get; set; }
+
+
+            public Coordinates()
+            {
+                this.Latitude = 0;
+                this.Longtitude = 0;
+            }
+
+            public Coordinates(double Latitude, double Longitude)
+            {
+                this.Latitude = Latitude;
+                this.Longtitude = Longtitude;
+            }
+
+
+        }
+    }
+}
---
+++ SampleSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleSignalR.Startup))]
+namespace SampleSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
