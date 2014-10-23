e08c7a130ca3e62374f13f845117d4da53efccd8
Simply007 <430596@mail.muni.cz>
2014-10-23 20:29:53 +0200

    Initial commit - angular sceleton

---
+++ FriendsTracker2/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace FriendsTracker2
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        private const string ROOT_DOCUMENT = "/index.html";
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            string url = Request.Url.LocalPath;
+            if (!System.IO.File.Exists(Context.Server.MapPath(url)))
+            {
+                Context.RewritePath(ROOT_DOCUMENT);
+            }
+        }
+
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{action}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+        }
+    }
+}
---
+++ FriendsTracker2/Scripts/Controllers/FriendsTrackerController.js
@@ -0,0 +1,20 @@
+﻿var friendsTrackerModule = angular.module('friendsTrackerModule', ['ngRoute']);
+
+friendsTrackerModule.controller('FriendsTrackerController', function ($scope) {
+    //Posible to add some data
+    $scope.testData = "Ahoj";
+});
+
+friendsTrackerModule.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.
+        when('/', {
+            templateUrl: 'Scripts/Templates/FriendsTrackerTemplate.html',
+            controller: 'FriendsTrackerController'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]
+);
+
